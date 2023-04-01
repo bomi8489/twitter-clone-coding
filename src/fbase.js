6b029b51f71a1ee1e54113b104f74244b0294ba8
AUTUMN <krshlee22@naver.com>
2023-04-01 21:17:43 +0900

    login

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,7 @@
 import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
+import {
+    getAuth,
+} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { 
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+ } from 'firebase/auth';
 
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+
+    const onChange = (e) => {
+        const {target: {name, value}} = e;
+        if(name === "email"){
+            setEmail(value);
+        } else if(name === "password") {
+            setPassword(value);
+        }
+    }
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        try{
+            let data;
+            const auth = getAuth();
+            if(newAccount){
+                data = await createUserWithEmailAndPassword(auth, email, password)
+            } else {
+                data = await signInWithEmailAndPassword(auth, email, password);
+            }
+            console.log(data);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <span>Auth</span>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="email" 
+                    type="email" 
+                    placeholder="Email" 
+                    value={email}
+                    onChange={onChange}
+                    required
+                />
+                <input 
+                    name="password" 
+                    type="password" 
+                    placeholder="Password" 
+                    value={password}
+                    onChange={onChange}
+                    required
+                />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
     )
 };
 
