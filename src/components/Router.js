58f87584a2d5d77bac93067dc1d85d1cd5bd78ca
AUTUMN <krshlee22@naver.com>
2023-04-02 20:20:21 +0900

    Log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,20 +2,30 @@ import React from "react";
 import {
     HashRouter as Router,
     Routes,
-    Route
+    Route,
+    Navigate
 } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from './../routes/Home';
+import Profile from "routes/Profile";
+import Auth from "routes/Auth";
+import Home from 'routes/Home';
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {/* navigation이 존재하려면 isLoggedIn이 true여야함 */}
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? 
                 <>
                     <Route path="/" element={<Home />}></Route>
+                    <Route path="/profile" element={<Profile />}></Route>
                 </>
-                :   <Route path="/" element={<Auth />}></Route>
+                :
+                <>
+                    <Route path="/" element={<Auth />}></Route>
+                    <Route path="*" element={<Navigate replace to="/" />}></Route>
+                </>
                 }
             </Routes>
         </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,14 @@
 import React from "react";
+import { authService } from 'fbase';
 
-export default () => <span>Auth</span>
+const Profile = () => {
+    const onLogOutClick = () => {
+        authService.signOut();
+    }
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+}
+export default Profile;
